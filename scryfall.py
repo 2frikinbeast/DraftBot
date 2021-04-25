872e0efab59b906e3077ed2c4c207d98fc54b59e
2frikinbeast <53154447+2frikinbeast@users.noreply.github.com>
2021-04-24 22:16:12 -0400

    Update scryfall.py

--- scryfall.py
+++ scryfall.py
@@ -75,3 +75,17 @@ def get_card_image(card: Card, return_url: bool = False, image_type: str = "png"
                 return Image.open(
                     requests.get(card.get_info()["card_faces"][int(not front_face)]["image_uris"][image_type],
                                  stream=True).raw)
+
+
+def get_set_list(set_id: str):
+    url = SCRYFALL_API + "/cards/search?q=is%3Abooster+e%3A" + set_id
+    card_list = []
+    while True:
+        raw_list = json.loads(api_get(url, response_type="text"))
+        for card_object in raw_list["data"]:
+            card = Card(card_object)
+            card_list.append(card)
+        if raw_list["has_more"]:
+            url = raw_list["next_page"]
+        else:
+            return card_list
